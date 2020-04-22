805deb7a9028990a87942e0c7b1e3889dcd8961e
kmbarnett <kmbarnett94@gmail.com>
2020-04-22 09:41:24 -0600

    Add post functionality

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,16 @@
-export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
+export const getOrders = async () => {
+  const response = await fetch('http://localhost:3001/api/v1/orders')
+  return await response.json()
+}
+
+export const postOrders = async (order) => {
+  const options = {
+    method: 'POST',
+    headers: {
+            'Content-Type': 'application/json'
+           },
+    body: JSON.stringify(order)
+  }
+  const response = await fetch('http://localhost:3001/api/v1/orders', options)
+  return await response.json()
 }
